dd71c03d4fb43a80f1c7cc222dea3b7e76a2d91e
Daiki Uehara <e245736@ie.u-ryukyu.ac.jp>
2025-01-15 04:10:33 +0900

    Mainまで実装　Logicクラスがまだ、斜め方向が未完成

--- app/src/main/java/jp/ac/uryukyu/ie/e245736/Logic.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245736/Logic.java
@@ -26,46 +26,62 @@ public class Logic {
     //コマが置けるか確認する
     public boolean check(int a, int b, int currentPlayerColor, int currentOpponentPlayer) {
         if(board[a][b] == 0) {
-            if(checkBoardEmptAndJudge(a, b, currentPlayerColor, currentOpponentPlayer)){
-                
+            if(checkBoardEmptyAndJudge(a, b, currentPlayerColor, currentOpponentPlayer)){
+                return true;
             }else{return false;}
         }else{return false;}
     }
 
     public void printBoard(){
         for(int i=0; i<8; i++){
-            System.out.println("--------");
+            System.out.println("----------------");
             for(int j=0; j<8; j++){
-                System.out.println("|");
+                System.out.print("|");
                 System.out.print(board[i][j]);
                 if(j==7) {
                     System.out.println("");
                 }
             }
         }
+        System.out.println("----------------");
     }
 
+    //if(checkJudge(a,b,player_color, currentOpponentPlayer)){
+    //  return true;
+    //}
+
     //ボードの盤面が空いているか確認しコマが置けるか判断する
-    private boolean checkBoardEmptAndJudge(int a, int b, int player_color, int currentOpponentPlayer) {
+    private boolean checkBoardEmptyAndJudge(int a, int b, int player_color, int currentOpponentPlayer) {
         //置くこま周辺上下左右斜め8つに相手のコマがあるかの確認
         if(board[a-1][b-1] == player_color || board[a][b-1] == player_color || board[a+1][b-1] == player_color || board[a-1][b] == player_color || 
         board[a+1][b] == player_color || board[a-1][b+1] == player_color || board[a][b+1] == player_color || board[a+1][b+1] == player_color){
-            if()
-        }el
+            if(checkJudge(a,b,player_color, currentOpponentPlayer)){
+                System.out.println("コマをひっくり返しました。");
+                return true;
+            }
+        }else{
+            System.out.printf("X座標:%d Y座標:%d にコマは置けません", a,b);
+            return false;
+        }
+        System.out.printf("X座標:%d Y座標:%d にコマは置けません", a,b);
+        return false;
     }
 
     //以下ひっくり返すか確認しひっくり返す
     public boolean checkJudge(int a, int b, int currentPlayerColor, int currentOpponentPlayerColor) {
-        
+        if(turnUp(a,b,currentPlayerColor,currentOpponentPlayerColor) || turnDown(a, b, currentPlayerColor, currentOpponentPlayerColor) || 
+        turnLeft(a,b,currentPlayerColor,currentOpponentPlayerColor) || turnRight(a,b,currentPlayerColor,currentOpponentPlayerColor)){
+            return true;
+        }else{return false;}
     }
 
     //上方向に相手のコマをひっくり返す
-    public void turnUp(int a, int b, int currentPlayerColor, int currentOpponentPlayerColor) {
+    public boolean turnUp(int a, int b, int currentPlayerColor, int currentOpponentPlayerColor) {
         int targetY = a-1;
 
         //盤の外なら終了
         if(targetY < 0) {
-            return;
+            return false;
         }
         
 
@@ -82,12 +98,108 @@ public class Logic {
 
             //もし自分のコマならば
             if(board[targetY][b] == currentPlayerColor) {
-                for(; a < targetY; ++){
-
+                for(; a < targetY; a--){
+                    board[a][b] = currentPlayerColor; 
                 }
-            }else{return;}
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //下方向に相手のコマをひっくり返す
+    public boolean turnDown(int a, int b, int currentPlayerColor, int currentOpponentPlayerColor){
+        //次のマスへ
+        int targetY = a+1;
+
+        //盤の外なら終了
+        if(targetY < 0){
+            return false;
         }
 
-        return;
+        //現在のプレイヤーではないコマでないorマスが空ではないならループし続ける
+        while (board[targetY][b] != currentPlayerColor && board[targetY][b] != 0) {
+            
+            //次のマスへ
+            targetY++;
+
+            //盤の外なら終了
+            if(targetY < 0){
+                break;
+            }
+
+            //もし自分のコマならば
+            if(board[targetY][b] == currentPlayerColor) {
+                for(; a < targetY; a++){
+                    board[a][b] = currentPlayerColor; 
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //右方向に相手のコマをひっくり返す
+    public boolean turnRight(int a, int b, int currentPlayerColor, int currentOpponentPlayerColor){
+        //コマを進める
+        int targetX = b+1;
+
+        //壁の外なら
+        if(targetX < 0){
+            return false;
+        }
+
+        //現在のプレイヤーではないコマでないorマスが空ではないならループし続ける
+        while(board[a][targetX] != currentPlayerColor && board[a][targetX] != 0){
+            //次のマスへ
+            targetX++;
+
+            //壁の外なら終了
+            if(targetX < 0){
+                break;
+            }
+
+            //もし自分のコマならば
+            if(board[a][targetX] == currentPlayerColor){
+                for(; b < targetX; b++){
+                    board[a][b] = currentPlayerColor;
+                }
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //左方向に相手のコマをひっくり返す
+    public boolean turnLeft(int a, int b, int currentPlayerColor, int currentOpponentPlayerColor){
+        //コマを進める
+        int targetX = b-1;
+
+        //壁の外なら
+        if(targetX < 0){
+            return false;
+        }
+
+        //現在のプレイヤーではないコマでないorマスが空ではないならループし続ける
+        while(board[a][targetX] != currentPlayerColor && board[a][targetX] != 0){
+            //コマを進める
+            targetX--;
+
+            //壁の外なら
+            if(targetX < 0){
+                break;
+            }
+
+            //もし自分のコマならば
+            if(board[a][targetX] == currentPlayerColor){
+                for(; b < targetX; b--){
+                    board[a][b] = currentPlayerColor;
+                }
+                return true;
+            }
+        }
+
+        return false;
     }
 }
--- app/src/main/java/jp/ac/uryukyu/ie/e245736/Main.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245736/Main.java
@@ -3,7 +3,7 @@ package jp.ac.uryukyu.ie.e245736;
 import java.util.Scanner;
 
 //0が何も乗ってなくて、１が白、２が黒
-public class Main {   
+public class Main {
     public static void main(String[] args) {
         int WhitePlayer = 1;
         int BlackPlayer = 2;
@@ -14,14 +14,33 @@ public class Main {
         int currentPlayer = WhitePlayer;
         System.out.println("先行は白です");
 
-        while(true) {    
-            logic.printBoard();
-            System.out.println("現在のターンは" + currentPlayer + "です");
-            System.out.println("どこにコマを置きますか？");
-            int row = scanner.nextInt();
-            int col = scanner.nextInt();
-            
+        while(true) {   
+            String currentColor = "";
 
+            if(currentPlayer == 1){
+                currentColor = "白";
+            }else if(currentPlayer == 2){
+                currentColor = "黒";
+            }
+
+            logic.printBoard();
+            System.out.println("現在のターンは" + currentColor + "です");
+            System.out.println("どこにコマを置きますか？");
+            System.out.println("X座標Y座標8x8で指定してください");
+            System.out.println("X座標を入力してくだい");
+            int row = scanner.nextInt() - 1;
+            System.out.println("Y座標を入力してください");
+            int col = scanner.nextInt() - 1;
+            
+            if(logic.check(col, row, currentPlayer, currentPlayer)){
+                if(currentPlayer == 1){
+                    currentPlayer = 2;
+                }else{
+                    currentPlayer = 1;
+                }
+            }else{
+                System.out.println("有効なマスにコマを置いてください!!!");
+            }
         }
     }
 }
